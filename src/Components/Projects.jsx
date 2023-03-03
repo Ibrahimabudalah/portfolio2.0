2e59c760f87d552df192bae987a174536f74e8a6
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-03-03 00:46:10 -0800

    linked live link to all projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import { ProjectsList } from "./ProjectsList";
 
 function Projects() {
-  console.log(ProjectsList[0].technologies);
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -22,15 +21,17 @@ function Projects() {
               key={i}
               className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 md:p-44 h-screen"
             >
-              <motion.img
-                initial={{ y: -100, opacity: 0 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1.2 }}
-                viewport={{ once: true }}
-                className=" h-28 xl:h-80 md:h-64 object-contain"
-                src={project.image}
-                alt={project.name}
-              />
+              <a href={project.liveLink}>
+                <motion.img
+                  initial={{ y: -100, opacity: 0 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1.2 }}
+                  viewport={{ once: true }}
+                  className=" h-28 xl:h-80 md:h-64 object-contain"
+                  src={project.image}
+                  alt={project.name}
+                />
+              </a>
 
               <div className="space-y-5 md:space-y-10 px-0 md:px-10 max-w-6xl">
                 <h4 className="text-lg md:text-2xl lg:text-4xl font-semibold text-center">
